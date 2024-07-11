3debb32a23494e5422b35477057dc750ebf1ff6e
Tallal Mohar <tallalmoohar@gmail.com>
2024-07-11 15:59:35 -0700

    upadted js

--- Express Server/index.js
+++ Express Server/index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 3000;
 
 
+
+app.get("/", (req,res) => {
+    res.send("HELLO WORLD");
+});
+
 app.listen(port, () =>{
     console.log(`Express port ${port}!`);
 });
